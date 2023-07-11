baeb6d002aa015351055b42ae9d19339eea2a087
joaonunatingscode <joaonunatingscode@gmail.com>
2023-07-12 00:07:45 +0100

    health and version endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
-const app = express();
-const port = process.env.PORT || 2000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+const app = express();
+const port: number = parseInt(process.env.PORT) || 2000;
+
+app.get("/health", (req, res) => {
+	console.log("Health check requested");
+	res.json({
+		status: "Healthy"
+	});
+});
+
+app.get("/version", (req, res) => {
+	console.log("Version requested");
+	res.json({
+		version: process.env.npm_package_version
+	});
 });
 
 app.listen(port, () => {
-	return console.log(`Express is listening at http://localhost:${port}`);
+	return console.log(`Running on port ${port}`);
 });
